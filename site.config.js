6eafad7f2caaccbd820c443fdad61843c138df07
Cd1s <135119404+Cd1s@users.noreply.github.com>
2023-06-06 16:46:04 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '6a76fdd9ec6d4ae2855aa790d620eec4',
+  rootNotionPageId: '612eb44c34e94695978fd2e8cdcdd7aa',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '菜饼的技术博客',
-  domain: 'szufrank.top',
-  author: '菜饼同学',
+  name: '奇客的博客',
+  domain: 'cheek.day',
+  author: '奇客同学',
 
   // open graph metadata (optional)
-  description: '菜饼同学: 专注硬核技术，校招打法，个人认知体系成长',
-  socialImageTitle: '菜饼同学',
+  description: '奇客: 专注个人认知，精神成长，人权',
+  socialImageTitle: '奇客同学',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
